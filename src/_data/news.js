41fac63acb0b2c9f925ce5cb7b2f765448b63806
Edgar Lechaudel <edgarlechaudel@MBP-de-Edgar.fritz.box>
2020-03-22 16:55:55 +0100

    handling news by country

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,32 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require('dotenv').config();
 
-module.exports = async function() {
+async function getNews(country) {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-21&sortBy=publishedAt&apiKey=${process.env.API_KEY}&pageSize=10`);
-    return response.data;
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=10`);
+    return {
+    	"country": country,
+    	"articles": response.data.articles
+    }
   } catch (error) {
     console.error(error);
   }
+ }
+
+module.exports = async function() {
+	var newsPromises = countries.map(getNews);
+	return Promise.all(newsPromises).then( newsObjects => {
+		console.log('newsObjects:', newsObjects);
+		return [].concat.apply([],newsObjects);
+	});
 }
+
+// module.exports = async function() {
+//   try {
+//     const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.API_KEY}&pageSize=10`);
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
