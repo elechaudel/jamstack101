04896641467797cbeb21c5fc94eeacf04f03d71f
Edgar Lechaudel <edgarlechaudel@MBP-de-Edgar.fritz.box>
2020-03-22 17:42:55 +0100

    Handling country redirects

--- src/_data/news.js
+++ src/_data/news.js
@@ -17,7 +17,6 @@ async function getNews(country) {
 module.exports = async function() {
 	var newsPromises = countries.map(getNews);
 	return Promise.all(newsPromises).then( newsObjects => {
-		console.log('newsObjects:', newsObjects);
 		return [].concat.apply([],newsObjects);
 	});
 }
