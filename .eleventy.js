102967da0bde2cb136696ae85b44c4a6bfe74a49
Edgar Lechaudel <edgarlechaudel@MBP-de-Edgar.fritz.box>
2020-04-15 20:32:02 +0200

    client side data and api

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function(config) {
 
+  	config.setBrowserSyncConfig({
+	    https: {
+	      key: 'etc/localhost.key',
+	      cert: 'etc/localhost.crt'
+	    }
+	 });
+
 	config.addPassthroughCopy("src/js");
 
 	return {
@@ -9,4 +16,5 @@ module.exports = function(config) {
 			data: "_data"
 		}
 	};
+
 };
